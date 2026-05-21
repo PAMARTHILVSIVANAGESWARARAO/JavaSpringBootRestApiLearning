f84f6d3383ef756e3d7b7a8d575a994ec70975c4
bhargav <bhargavbala56@gmail.com>
2026-05-21 23:27:56 +0530

    Learned about the CSR

---
+++ src/main/java/com/siva/restlearning/controller/CSR_Controller.java
@@ -0,0 +1,21 @@
+package com.siva.restlearning.controller;
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/csr")
+public class CSR_Controller
+{
+    CSR_Service service =
+            new CSR_ServiceImpl();
+    @GetMapping("/all")
+    public ArrayList<HashMap<String,Integer>>
+    showStudents()
+    {
+        return service.displayAllStudents();
+    }
+    @GetMapping("/count")
+    public int countStudents()
+    {
+        return service.totalStudents();
+    }
+}
---
+++ src/main/java/com/siva/restlearning/controller/CSR_RepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.siva.restlearning.controller;
+import java.util.*;
+import org.springframework.stereotype.Repository;
+interface CSR_Repository
+{
+    ArrayList<HashMap<String,Integer>> getData();
+}
+@Repository
+public class CSR_RepositoryImpl
+implements CSR_Repository
+{
+    public ArrayList<HashMap<String,Integer>> getData()
+    {
+        HashMap<String,Integer> hm1 =
+                new HashMap<>();
+        hm1.put("Rahul",20);
+        HashMap<String,Integer> hm2 =
+                new HashMap<>();
+        hm2.put("Priya",21);
+        HashMap<String,Integer> hm3 =
+                new HashMap<>();
+        hm3.put("Arjun",22);
+        ArrayList<HashMap<String,Integer>> students=
+                new ArrayList<>();
+        students.add(hm1);
+        students.add(hm2);
+        students.add(hm3);
+        return students;
+
+    }
+
+}
