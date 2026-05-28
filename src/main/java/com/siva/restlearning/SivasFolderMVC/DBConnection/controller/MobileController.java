15fa056a0004ce254ce42a75f2386c6b9e7acf34
PAMARTHILVSIVANAGESWARARAO <24a85a0506@sves.org.in>
2026-05-28 20:30:11 +0530

    Add methods for batch insertion, updating, and deletion of mobiles in MobileService and MobileController

--- src/main/java/com/siva/restlearning/SivasFolderMVC/DBConnection/controller/MobileController.java
+++ src/main/java/com/siva/restlearning/SivasFolderMVC/DBConnection/controller/MobileController.java
@@ -3,11 +3,14 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +45,31 @@ public class MobileController {
     public String addMobileByQuery(@RequestBody @NonNull Mobile mobile) {
         return mobileService.addMobileByQuery(mobile);
 
+    }
+
+    @PostMapping("insertManybyQuery")
+    public ResponseEntity<?> insertManyMobilesByQuery(@RequestBody List<Mobile> mobiles) {
+        
+       
+
+        for (Mobile mobile : mobiles) {
+            mobileService.addMobileByQuery(mobile);
+        }
+
+        // Same ResponseEntity Can Be Created Using ResponseEntity.ok() Method Which Sets The Status To 200 Automatically
+
+        // return new ResponseEntity<>("Mobiles added successfully", HttpStatus.OK);
+        return ResponseEntity.status(200).body("Mobiles added successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateMobile(@PathVariable @NonNull Long id, @RequestBody Mobile mobile) {
+        return mobileService.updateMobile(id, mobile);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteMobile(@PathVariable @NonNull Long id) {
+        return mobileService.deleteMobile(id);
     }
 
     
